5e147009e3e3ea3f9d726a10093ddb82b9b2477a
Mako0530 <zhongtiancheng79@gmail.com>
2022-06-15 22:36:39 +0900

    Five additions

--- Kadai1/ViewController.swift
+++ Kadai1/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
+    
+    
+    @IBOutlet weak var label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapButton(_ sender: Any) {
+        //入力
+            let number1 = Int(textField1.text ?? "") ?? 0
+            let number2 = Int(textField2.text ?? "") ?? 0
+            let number3 = Int(textField3.text ?? "") ?? 0
+            let number4 = Int(textField4.text ?? "") ?? 0
+            let number5 = Int(textField5.text ?? "") ?? 0
+        
+        //処理
+            let result = number1 + number2 + number3 + number4 + number5
+        
+        //出力
+        label.text = "\(result)"
+    }
 
+    
+    
 }
 
